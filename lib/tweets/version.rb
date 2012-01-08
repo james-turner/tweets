ced916ca39e080faf2e56be3b94a132b635e4e5d
James Turner <james.turner.phpninja@gmail.com>
2012-01-08 11:35:30 +0000

    Upped the version number now that i've put in the exception catching and throwing. Revmoed the begin/end parts from the README

--- lib/tweets/version.rb
+++ lib/tweets/version.rb
@@ -2,9 +2,9 @@ module Tweets
   module Version
     MAJOR = 0
     MINOR = 0
-    PATCH = 1
-    BUILD = "pre2"
-    STRING = [MAJOR, MINOR, PATCH, BUILD].join('.')
+    PATCH = 2
+    BUILD = ""
+    STRING = [MAJOR, MINOR, PATCH, BUILD].select{|version| version.to_s.length > 0}.join('.')
   end
 
   def self.version
